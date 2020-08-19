0e0c367fc22c47141bdad5ac976080b65debcb83
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-08-18 21:13:49 -0300

    comparando resultados

--- com/company/comparacao.java
+++ com/company/comparacao.java
@@ -1,7 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class comparacao {
     public static void main(String[] args) {
+        Scanner dados = new Scanner(System.in);
 
+        float a1 = (4/2)+(2/4);
+        float a2 = 4/2+2/4;
+
+        float b1 = 4/(2+2)/4;
+        float b2 = 4/2+2/4;
+
+        float c1 = (4+2)*2-4;
+        float c2 = 4+2*2-4;
+
+        boolean a = (a1==a2);
+        boolean b = (b1==b2);
+        boolean c = (c1==c2);
+
+        System.out.println("Os resultado das conta são: ");
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println("Os dois resultados são iguais ?");
+        System.out.println(a);
+
+        System.out.println("Os resultado das conta são: ");
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println("Os dois resultados são iguais ?");
+        System.out.println(b);
+
+        System.out.println("Os resultado das conta são: ");
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println("Os dois resultados são iguais ?");
+        System.out.println(c);
     }
 }
